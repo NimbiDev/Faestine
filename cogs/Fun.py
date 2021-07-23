c9b11e61c55afac2889e75146154fe12eb499a13
Joshua Lewis <JoshuaVL1988@pm.me>
2021-07-22 20:35:44 -0700

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -12,8 +12,10 @@ load_dotenv()
 PREFIX = os.getenv('COMMAND_PREFIX')
 OSU_API_TOKEN = os.getenv('OSU_TOKEN')
 GW2_API_TOKEN = os.getenv('GW2_TOKEN')
+TENOR_API_TOKEN = os.getenv('TENOR_TOKEN')
+GIPHY_API_TOKEN = os.getenv('GIPHY_TOKEN')
 
-TOKENS = {'TENOR_API': os.getenv('TENOR_TOKEN'), 'GIPHY_API': os.getenv('GIPHY_TOKEN')}
+TOKENS = {'TENOR_API': GIPHY_API_TOKEN, 'GIPHY_API': GIPHY_API_TOKEN}
 
 TENOR = TenGiphPy.Tenor(token=TOKENS['TENOR_API'])
 GIPHY = TenGiphPy.Giphy(token=TOKENS['GIPHY_API'])
@@ -26,13 +28,13 @@ class Fun(commands.Cog):
     @commands.command(aliases=['t'], description='Return a random gif by tag from tenor.', usage=f'Usage: {PREFIX}tenor [query]\nExample: {PREFIX}tenor dog')
     @commands.has_guild_permissions(send_messages=True, embed_links=True)
     async def tenor(self, ctx, *, text):
-        response = await TENOR.arandom(str(text))
+        response = await TENOR.random(str(text))
         await ctx.send(f'{response}')
 
     @commands.command(aliases=['g'], description='Return a random gif by tag from giphy.', usage=f'Usage: {PREFIX}giphy [query]\nExample: {PREFIX}giphy dog')
     @commands.has_guild_permissions(send_messages=True, embed_links=True)
     async def giphy(self, ctx, *, text):
-        response = await GIPHY.arandom(str(text))
+        response = await GIPHY.random(str(text))
         await ctx.send(f'{response}')
 
 
