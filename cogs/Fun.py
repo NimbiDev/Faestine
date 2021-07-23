a38ff7565a21e3d0f398fd6eb1da8fbd27ed52b9
Joshua Lewis <JoshuaVL1988@pm.me>
2021-07-22 21:23:18 -0700

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,44 +1,41 @@
 import discord
 import os
-import aiohttp
-import random
-import json
+import TenGiphPy
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+
 PREFIX = os.getenv('COMMAND_PREFIX')
+OSU_API_TOKEN = os.getenv('OSU_TOKEN')
+GW2_API_TOKEN = os.getenv('GW2_TOKEN')
 TENOR_API_TOKEN = os.getenv('TENOR_TOKEN')
+
+TOKENS = {'TENOR_API': TENOR_API_TOKEN}
+
+TENOR = TenGiphPy.Tenor(token=TOKENS['TENOR_API'])
 
 
 class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['g'], description='Get a random gif from tenor via tag', usage=f'Usage: {PREFIX}gif [tag]\nExample: {PREFIX}gif dog')
+    @commands.command(aliases=['t'], description='Return a random gif by tag from tenor.', usage=f'Usage: {PREFIX}tenor [query]\nExample: {PREFIX}tenor dog')
     @commands.has_guild_permissions(send_messages=True, embed_links=True)
-    async def gif(self, ctx, query):
-        async with aiohttp.ClientSession() as session:
-            embed = discord.Embed(
-                colour=discord.Colour.dark_red())
-            response = await session.get(f'https://api.tenor.com/v1/search?q={query}&key={TENOR_API_TOKEN}&limit=30&media_filter=basic')
-            data = json.loads(await response.text())
-            gif_choice = random.randint(0, 29)
+    async def tenor(self, ctx, *, _text):
 
-            for result in data['results']:
-                print('- result -')
-                # print(result)
+        _url = await TENOR.arandom(str(_text))
 
-                for media in result['media']:
-                    print('- media -')
-                    # print(media)
-                    # print(media['gif'])
-                    print('url:', media['gif']['url'])
-
-                    embed.set_image(url=data['results'][0][gif_choice]['gif']['url'])
-
-        await session.close()
+        embed = discord.Embed(colour=discord.Colour.random())
+        embed.set_image(url=f'{_url}')
         await ctx.send(embed=embed)
+
+    @tenor.error
+    async def tenor_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(':x: Tag cant be None. Please give a valid tag to search.')
+        else:
+            raise error
 
 
 def setup(client):
