f0a6c44f69a6c81a976de74f392c072833b69a90
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-09 00:38:43 -0700

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -60,58 +60,58 @@ class Events(commands.Cog):
 
         if isinstance(error, commands.DisabledCommand):
             print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-            await ctx.send('The command {} has been disabled.'.format(ctx.command))
+            await ctx.reply('The command {} is disabled.'.format(ctx.command))
+            await log_error
 
         elif isinstance(error, commands.NoPrivateMessage):
             try:
                 print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
-                traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
                 await ctx.author.send('The command {} can not be used in Private Messages.'.format(ctx.command))
+                await log_error
             except discord.HTTPException:
                 pass
 
         elif isinstance(error, commands.CommandError):
             if ctx.command.qualified_name == ctx.command:
                 print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
+                await ctx.reply('There was a problem with that command...')
                 await log_error
-                await ctx.send('There was a problem with that command...')
 
         elif isinstance(error, commands.BotMissingPermissions):
             if ctx.command.qualified_name == ctx.command:
                 print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
+                await ctx.reply('I do not have permission to use the command: {}'.format(ctx.command))
                 await log_error
-                await ctx.send('I do not have permission to use the command: {}'.format(ctx.command))
 
         elif isinstance(error, commands.MissingPermissions):
             if ctx.command.qualified_name == ctx.command:
                 print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
+                await ctx.reply('You do not have permission to use the command {}'.format(ctx.command))
                 await log_error
-                await ctx.send('You do not have permission to use the command {}'.format(ctx.command))
 
         elif isinstance(error, commands.MissingRequiredArgument):
             if ctx.command.qualified_name == ctx.command:
                 print('There was an exception in command {}:'.format(ctx.command), file=sys.stderr)
+                await ctx.reply('Please pass in all required argument(s)... Type {}help {} for help.'.format(self.clean_prefix, ctx.command))
                 await log_error
-                await ctx.send('Please pass in all required argument(s)... Type {}help {} for help.'.format(self.clean_prefix, ctx.command))
 
         elif isinstance(error, commands.ThreadNotFound):
             error_channel = self.bot.get_channel(ERROR_CHANNEL)
             print('I can not find the specified thread.', file=sys.stderr)
-            await log_error
             await error_channel.send('An Error Has Occurred {}:'.format(error), file=sys.stderr)
+            await log_error
 
         elif isinstance(error, commands.ChannelNotFound):
             error_channel = self.bot.get_channel(ERROR_CHANNEL)
             print('I can not find the specified channel.', file=sys.stderr)
-            await log_error
             await error_channel.send('An Error Has Occurred {}:'.format(error), file=sys.stderr)
+            await log_error
 
         elif isinstance(error, commands.UserNotFound):
             error_channel = self.bot.get_channel(ERROR_CHANNEL)
             print('I can not find the specified user.', file=sys.stderr)
-            await log_error
             await error_channel.send('An Error Has Occurred {}:'.format(error), file=sys.stderr)
+            await log_error
 
         else:
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
