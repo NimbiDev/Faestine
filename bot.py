79da9557812fb75b568c4cac579a6f7e7dedf322
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-09 03:52:01 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,8 @@ class HelpEmbed(commands.HelpCommand):
             if command_signatures:
                 cog_name = getattr(cog, 'qualified_name', 'Other')
                 sig = '\n'.join(command_signatures)
-                embed.add_field(name='{} Commands'.format(cog_name), value='```xml\n{}```'.format(sig), inline=False)
+                embed.add_field(name='{} Commands'.format(cog_name), value='```xml\n{}```'.format(sig), inline=True)
+                embed.set_thumbnail(url=self.bot.user.avatar)
                 embed.set_image(url='https://www.autoitscript.com/forum/uploads/monthly_2020_03/tech.gif.7449db47191b0e32967887c117908b3c.gif')
                 embed.set_footer(text='{} | {}'.format(self.bot.user.name, url), icon_url=self.bot.user.avatar)
         channel = self.get_destination()
