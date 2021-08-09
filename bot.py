6af70fec95b575756bc1d18c5e82f5d1ec8f1709
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-08 22:42:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,13 +10,13 @@ PREFIX = os.getenv('COMMAND_PREFIX')
 
 
 class HelpEmbed(commands.HelpCommand):
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help")
-        for cog, commands in mapping.items():
-            command_signatures = [self.get_command_signature(c) for c in commands]
-            if command_signatures:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command))
+        embed.add_field(name="Help", value=command.help)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
         channel = self.get_destination()
         await channel.send(embed=embed)
 
