d2a844b0eb84b42e11779b5b3bfb78a8bddf41a6
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-08 23:15:32 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,28 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(commands_prefix=commands.when_mentioned_or('fae '))
+bot = commands.Bot(command_prefix="fae ")
 
 
 class HelpEmbed(commands.HelpCommand):
+    def __init__(self, **options):
+        super().__init__(options)
+        self.clean_prefix = None
+
     def get_command_signature(self, command):
         return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help")
+        emb = discord.Embed(title="Help")
         for cog, command in mapping.items():
             filtered = await self.filter_commands(command, sort=True)
             command_signatures = [self.get_command_signature(c) for c in filtered]
             if command_signatures:
                 cog_name = getattr(cog, "qualified_name", "Other")
-                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+                emb.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
 
         channel = self.get_destination()
-        await channel.send(embed=embed)
+        await channel.send(embed=emb)
 
 
 bot.help_command = HelpEmbed()
