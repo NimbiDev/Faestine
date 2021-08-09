45915f5ea87cc5035e4584666e9e0771278e6e70
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-09 03:05:23 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,8 @@ class HelpEmbed(commands.HelpCommand):
                 cog_name = getattr(cog, 'qualified_name', 'Other')
                 sig = '\n'.join(command_signatures)
                 embed.add_field(name='{} Commands'.format(cog_name), value='```xml\n{}```'.format(sig), inline=False)
-                embed.set_footer(text=self.bot.user.name)
+                embed.colour(discord.Colour.blue())
+                embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar.url)
         channel = self.get_destination()
         await channel.send(embed=embed)
 
