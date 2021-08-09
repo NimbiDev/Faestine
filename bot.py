07532486617550e8995686a571e808de099dfba8
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-09 03:04:34 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,7 @@ class HelpEmbed(commands.HelpCommand):
                 cog_name = getattr(cog, 'qualified_name', 'Other')
                 sig = '\n'.join(command_signatures)
                 embed.add_field(name='{} Commands'.format(cog_name), value='```xml\n{}```'.format(sig), inline=False)
-                embed.colour(discord.Colour.blue())
-                embed.timestamp()
+                embed.set_footer(text=self.bot.user.name)
         channel = self.get_destination()
         await channel.send(embed=embed)
 
