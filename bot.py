e24baefed513067c778d8064dff3e3bf0b270de1
Joshua Lewis <JoshuaVL1988@pm.me>
2021-08-09 03:02:00 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ class HelpEmbed(commands.HelpCommand):
                 embed.add_field(name='{} Commands'.format(cog_name), value='```xml\n{}```'.format(sig), inline=False)
                 embed.colour(discord.Colour.blue())
                 embed.timestamp()
-                embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar)
+                embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar.url)
         channel = self.get_destination()
         await channel.send(embed=embed)
 
