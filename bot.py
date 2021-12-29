49a3fb607fa1244e1db794ea9b6242eafdbfe61d
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-29 04:02:09 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,43 +20,11 @@ client = commands.Bot(
 )
 
 
-for filename in os.listdir('./cogs/admin'):
+for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.admin.{filename[:-3]}')
+        client.load_extension(f'cogs.{filename[:-3]}')
     else:
         print(f'Unable to load {filename[:-3]}')
 
-for filename in os.listdir('./cogs/mod'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.mod.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-
-for filename in os.listdir('./cogs/util'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.utl.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-
-for filename in os.listdir('./cogs/events'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.events.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-for filename in os.listdir('./cogs/social'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.social.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-for filename in os.listdir('./cogs/image'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.image.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-for filename in os.listdir('./cogs/owner'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.owner.{filename[:-3]}')
-    else:
-        print(f'Unable to load {filename[:-3]}')
-
+        
 client.run(TOKEN)
