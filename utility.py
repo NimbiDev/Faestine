5c86bbc1066352c562958354335e1334461ba059
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-29 05:34:37 -0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -7,11 +7,6 @@ class Utility(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name='user-info', aliases=['user'], description='Display information about a specified member.')
-    async def _userinfo(self, ctx, member):
-        msg = f'{member} joined on {member.joined_at} and has {len(member.roles)} roles.'
-        await ctx.send(msg)
-
     @commands.command(name='ping', aliases=['echo', 'beep'], description='Simple Ping Pong command.')
     async def _ping(self, ctx):
         await ctx.send('Pong!')
