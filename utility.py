08dd0e37d29c8660ea154cc434855b227ef0ae66
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 11:02:09 -0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -9,7 +9,18 @@ class Utility(commands.Cog):
 
     @commands.command(name='ping', aliases=['echo', 'beep'], description='Simple Ping Pong command.')
     async def _ping(self, ctx):
-        await ctx.send('Pong!')
+        # await ctx.send('Pong!')
+        client = self.client
+        
+        if round(client.latency * 1000) <= 50:
+            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x44ff44)
+        elif round(client.latency * 1000) <= 100:
+            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xffd000)
+        elif round(client.latency * 1000) <= 200:
+            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xff6600)
+        else:
+            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x990000)
+        await ctx.send(embed=embed)
 
 
 def setup(client):
