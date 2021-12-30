82ecb84ebb499dd873cb7a1aed0285bfe133d6ec
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 11:06:38 -0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -7,19 +7,33 @@ class Utility(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name='ping', aliases=['echo', 'beep'], description='Simple Ping Pong command.')
+    @commands.command(name='ping', aliases=['echo', 'beep'], description='Returns the bot\'s latency.')
     async def _ping(self, ctx):
-        # await ctx.send('Pong!')
         client = self.client
-        
         if round(client.latency * 1000) <= 50:
-            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x44ff44)
+            embed = discord.Embed(
+                title="PING",
+                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                color=0x44ff44
+            )
         elif round(client.latency * 1000) <= 100:
-            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xffd000)
+            embed = discord.Embed(
+                title="PING",
+                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                color=0xffd000
+            )
         elif round(client.latency * 1000) <= 200:
-            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xff6600)
+            embed = discord.Embed(
+                title="PING",
+                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                color=0xff6600
+            )
         else:
-            embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x990000)
+            embed = discord.Embed(
+                title="PING",
+                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                color=0x990000
+            )
         await ctx.send(embed=embed)
 
 
