3984f0b1a0fe16cb592e845e931a5a63c2fcb2fb
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 11:33:01 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,32 +36,14 @@ class CustomHelp(commands.MinimalHelpCommand):
 
 client.help_command = CustomHelp()
 
-
-# extensions = [
-#     'cogs.admin',
-#     'cogs.mod',
-#     'cogs.image',
-#     'cogs.events',
-#     'cogs.utility',
-#     'cogs.social'
-# ]
-
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
         print(f'Loaded cogs.{filename[:-3]}')
+        print('------')
     else:
         print(f'Unable to load cogs.{filename[:-3]}')
+        print('------')
 
-# for extension in extensions:
-#     try:
-#         client.load_extension(extension)
-#         print('{} extension loaded.'.format(extension))
-#     except Exception as error:
-#         print('{} can not be loaded. [{}]'.format(extension, error))
-#
-# if __name__ == '__main__':
-#     client.run('{}'.format(TOKEN))
 
 client.run('{}'.format(TOKEN))
