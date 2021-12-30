3cf1af7444d8fbad348dd530cf3bd60d1c0bd563
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 11:34:57 -0800

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -13,25 +13,25 @@ class Utility(commands.Cog):
         if round(client.latency * 1000) <= 50:
             embed = discord.Embed(
                 title="PING",
-                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                description=f":ping_pong: The ping is **{round(client.latency *1000)}** milliseconds!",
                 color=0x44ff44
             )
         elif round(client.latency * 1000) <= 100:
             embed = discord.Embed(
                 title="PING",
-                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                description=f":ping_pong: The ping is **{round(client.latency *1000)}** milliseconds!",
                 color=0xffd000
             )
         elif round(client.latency * 1000) <= 200:
             embed = discord.Embed(
                 title="PING",
-                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                description=f":ping_pong: The ping is **{round(client.latency *1000)}** milliseconds!",
                 color=0xff6600
             )
         else:
             embed = discord.Embed(
                 title="PING",
-                description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!",
+                description=f":ping_pong: The ping is **{round(client.latency *1000)}** milliseconds!",
                 color=0x990000
             )
         await ctx.send(embed=embed)
