a16030128b55c22f63855510a3b287ef3b70fdc0
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 11:45:14 -0800

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -1,12 +1,46 @@
+import os
+import aiohttp
+import random
 import discord
-import asyncio
+import json
 from discord.ext import commands
+from dotenv import load_dotenv
 
-class Images(commands.Cog):
+load_dotenv()
+
+prefix = os.getenv("CLIENT_PREFIX")
+giphy_api = os.getenv("GIPHY_API_KEY")
+
+command_attrs = {'hidden': False}
+
+
+class Images(commands.Cog, name='Fun Commands', command_attrs=command_attrs):
     def __init__(self, client):
         self.client = client
+        self.session = aiohttp.ClientSession()
 
-        # Insert Code Here
+    def cog_unload(self):
+        self.client.loop.create_task(self.session.close())
+
+    @commands.command(name='gif')
+    async def _gif(self, ctx, *, search):
+        session = self.session
+        embed = discord.Embed(colour=discord.Color.dark_gold())
+
+        if search == '':
+            response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=' + giphy_api)
+            data = json.loads(await response.text())
+            embed.set_image(url=data['data']['images']['original']['url'])
+        else:
+            search.replace(' ', '+')
+            response = await session.get(
+                'http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + giphy_api + '&limit=10')
+            data = json.loads(await response.text())
+            gif_choice = random.randint(0, 9)
+            embed.set_image(url=data['data'][gif_choice]
+                            ['images']['original']['url'])
+            await ctx.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(Images(client))
