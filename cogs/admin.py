99b386fd66f4b773fba1c949da48d1431d79c560
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 13:59:41 -0800

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -17,22 +17,6 @@ class Admin(commands.Cog, description='Administration commands to help manage yo
             description=f'Successfully deleted {len(deleted)} messeges.', color=discord.Color.green())
         await ctx.send(embed=embed, mention_author=False, delete_after=5)
 
-    @commands.command(name='banlist', aliases=['blist'], description='Get a list of banned users in the guild.')
-    @commands.has_permissions(administrator=True, ban_members=True)
-    async def banlist(self, ctx):
-        client = self.client
-        message = ctx.message
-        orange = discord.Color.orange()
-
-        bans = await client.get_bans(message.guild.id)
-        for ban in bans:
-            user = ban['user']
-            reason = ban['reason']
-
-        embed = discord.Embed(
-            description='**{}**: {}'.format(user, reason), color=orange)
-        await ctx.send(embed=embed, mention_author=False)
-
 
 def setup(client):
     client.add_cog(Admin(client))
