995ff0def459f16f62e9a1b19b3b97b405ccaa5a
Nimbi <JoshuaVL1988@protonmail.com>
2021-12-30 12:07:17 -0800

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,11 +2,22 @@ import discord
 import asyncio
 from discord.ext import commands
 
+
 class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-        # Insert Code Here
+    @commands.command(name="purge", aliases=['fprune', 'fclean', 'fdelete'], description='Delete a specified number of messeges from the channel.')
+    async def purge(self, ctx, amount: int):
+        deleted = await ctx.channel.purge(limit=amount)
+        e = discord.embed(
+            title='Purge',
+            description='Successfully deleted {} messeges.'.format(
+                len(deleted)),
+            color=discord.Color.green()
+        )
+        await ctx.send(embed=e, mention_auther=False)
+
 
 def setup(client):
     client.add_cog(Admin(client))
