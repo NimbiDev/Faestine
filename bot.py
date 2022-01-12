97bb656ee8deb3062508594e1c936247e3881669
Nimbi <JoshuaVL1988@protonmail.com>
2022-01-12 13:56:40 -0700

    Update command handler

--- bot.py
+++ bot.py
@@ -47,61 +47,61 @@ client.remove_command('help')
 
 # client.help_command = CustomHelp()
 
-# for filename in os.listdir('./cogs'):
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension('cogs.{}'.format(filename[:-3]))
+        print('Loaded cogs.{}'.format(filename[:-3]))
+    else:
+        print('Unable to load cogs.{}'.format(filename[:-3]))
+        
+# for filename in os.listdir('./cogs/admin'):
 #     if filename.endswith('.py'):
 #         client.load_extension('cogs.{}'.format(filename[:-3]))
 #         print('Loaded cogs.{}'.format(filename[:-3]))
 #     else:
 #         print('Unable to load cogs.{}'.format(filename[:-3]))
         
-for filename in os.listdir('./cogs/admin'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
-        
-for filename in os.listdir('./cogs/mod'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/mod'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
-for filename in os.listdir('./cogs/events'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/events'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
-for filename in os.listdir('./cogs/images'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/images'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
-for filename in os.listdir('./cogs/owner'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/owner'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
-for filename in os.listdir('./cogs/social'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/social'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
-for filename in os.listdir('./cogs/util'):
-    if filename.endswith('.py'):
-        client.load_extension('cogs.{}'.format(filename[:-3]))
-        print('Loaded cogs.{}'.format(filename[:-3]))
-    else:
-        print('Unable to load cogs.{}'.format(filename[:-3]))
+# for filename in os.listdir('./cogs/util'):
+#     if filename.endswith('.py'):
+#         client.load_extension('cogs.{}'.format(filename[:-3]))
+#         print('Loaded cogs.{}'.format(filename[:-3]))
+#     else:
+#         print('Unable to load cogs.{}'.format(filename[:-3]))
 
 error_logger = logging.getLogger('discord')
 debug_logger = logging.getLogger('discord')
