72024dafb66cc9c7ffb7b52c9086ef43ca2097d2
Nimbi <JoshuaVL1988@protonmail.com>
2022-01-12 13:54:00 -0700

    Create purge.py

---
+++ cogs/admin/purge.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+
+
+class Purge(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(name="purge", aliases=['fprune', 'fclean', 'fdelete'], description='Delete a specified number of messeges from the channel.')
+    @commands.has_permissions(administrator=True, manage_messages=True)
+    async def purge(self, ctx, amount: int):
+        await ctx.message.delete()
+        deleted = await ctx.channel.purge(limit=amount)
+        embed = discord.Embed(
+            description=f'Successfully deleted {len(deleted)} messeges.', color=discord.Color.green())
+        await ctx.send(embed=embed, mention_author=False, delete_after=5)
+
+
+def setup(client):
+    client.add_cog(Purge(client))
