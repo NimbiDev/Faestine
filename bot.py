a2e27fd0ec2fe18515b50345a9cd508b068231ea
Nimbi <JoshuaVL1988@protonmail.com>
2022-01-12 18:39:21 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,15 +29,11 @@ class Help(commands.Cog):
 class CustomHelp(commands.HelpCommand):
     async def send_bot_help(self, mapping):
         embed = discord.Embed(title="Command help")
-        # `mapping` is a dict of the bot's cogs, which map to their commands
-        for cog, commands in mapping.items():  # get the cog and its commands separately
+        for cog, cmds in mapping.items():
             embed.set_thumbnail(url='https://raw.githubusercontent.com/DevCorner-Github/Faestine/main/assets/logo.png')
-            embed.add_field(
-                name = cog.qualified_name,       # get the cog name
-                value = f"{len(commands)} commands"  # get a count of the commands in the cog.
-            )
+            embed.add_field(name = cog.qualified_name, value = f"{len(cmds) commands}")
             
-        channel = self.get_destination()  # this method is inherited from `HelpCommand`, and gets the channel in context
+        channel = self.get_destination()
         await channel.send(embed=embed)
 
 client.help_command = CustomHelp()
